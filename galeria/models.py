204d583298b38e39b98faa7feabfda5135312427
felipemm-santos <tfelipesantos99s@gmail.com>
2025-12-13 19:26:04 -0300

    fix: media url

--- galeria/models.py
+++ galeria/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Fotografias (models.Model):
-    titulo = models.CharField(max_length=200)
-    descricao = models.TextField(blank=True)
-    imagem = models.ImageField(upload_to='fotos/')
+    titulo = models.CharField(max_length=200,null=False, blank=False)
+    descricao = models.TextField(blank=True, null=True)
+    imagem = models.ImageField(upload_to='fotos/%Y/%m/%d/', blank=True)
     data_publicacao = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
--- setup/settings.py
+++ setup/settings.py
@@ -20,7 +20,6 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
@@ -125,6 +124,9 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'setup/static')]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
