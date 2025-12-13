cf8337de05f1c375c3eb45eba91980e16c20c9be
felipemm-santos <tfelipesantos99s@gmail.com>
2025-12-13 20:54:35 -0300

    fix: problems with information display

--- galeria/views.py
+++ galeria/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
-from galeria.models import Fotografias
+from django.shortcuts import get_object_or_404, render
+from galeria.models import Fotografia
 
 def galeria_view(request):
-    fotos = Fotografias.objects.filter(publicada=True).order_by('-data_publicacao')
-    return render(request, 'galeria/index.html', {'cards': fotos})    
+    fotos = Fotografia.objects.filter(publicada=True).order_by('-data_publicacao')
+    return render(request, 'galeria/index.html', {'fotografias': fotos})    
 
-def imagem_view(request):
-    return render(request, 'galeria/imagem.html')
+def imagem_view(request, foto_id):
+    fotografia = get_object_or_404(Fotografia, pk=foto_id)
+    return render(request, 'galeria/imagem.html', {'fotografia': fotografia})
