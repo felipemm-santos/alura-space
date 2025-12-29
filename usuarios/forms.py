ce6f28ea2ccfb486ed6a86dd96823f51f8b93c69
felipemm-santos <tfelipesantos99s@gmail.com>
2025-12-28 21:41:17 -0300

    refactor: reduce repeaated code using django forms

---
+++ usuarios/forms.py
@@ -0,0 +1,71 @@
+from django import forms
+
+class LoginForms(forms.Form):
+    nome_login=forms.CharField(
+        label='Nome de Login', 
+        required=True, 
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Digite o seu nome de login',
+            }
+        )
+    )
+    senha=forms.CharField(
+        label='Senha', 
+        required=True, 
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Digite a sua senha',
+            }
+        )
+    )
+
+class CadastroForms(forms.Form):
+    nome_cadastro=forms.CharField(
+        label='Nome de Cadastro', 
+        required=True, 
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Digite o seu nome de cadastro',
+            }
+        )
+    )
+    email=forms.EmailField(
+        label='E-mail', 
+        required=True, 
+        max_length=100,
+        widget=forms.EmailInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Digite o seu e-mail',
+            }
+        )
+    )
+    senha=forms.CharField(
+        label='Senha', 
+        required=True, 
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Digite a sua senha',
+            }
+        )
+    )
+    confirma_senha=forms.CharField(
+        label='Confirmação de Senha', 
+        required=True, 
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Confirme a sua senha',
+            }
+        )
+    )
