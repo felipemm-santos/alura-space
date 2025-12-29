4fba0b610ad05f88595dbf6931218b12bfb51fc1
felipemm-santos <tfelipesantos99s@gmail.com>
2025-12-28 21:47:42 -0300

    fix: fixing error on classes for messeges

--- setup/settings.py
+++ setup/settings.py
@@ -133,3 +133,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+    messages.SUCCESS: 'success'
+}
